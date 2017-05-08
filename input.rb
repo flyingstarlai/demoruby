0570c1c16b61342bb1b630134ffafd562aeba04f
starlai <flyingstarlai@gmail.com>
2017-05-08 20:42:22 +0800

    maenan kata kunci

---
+++ input.rb
@@ -0,0 +1,16 @@
+# puts "Masukan nama: "
+# nama = gets.chomp
+
+# p nama
+
+puts "Sapa ini?"
+nama = gets.chomp
+
+puts "Masukkan kata kunci"
+katakunci = gets.chomp
+
+if (katakunci == "1234" )
+    puts "halo " +  nama
+else 
+    puts "sapa ya?"
+end
