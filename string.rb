1418a36a59008030b68513275934f96f64920b83
starlai <flyingstarlai@gmail.com>
2017-05-08 20:59:10 +0800

    string

---
+++ string.rb
@@ -0,0 +1,18 @@
+# interpolation
+puts "Pesan apa?"
+menu = gets.chomp
+puts "Berapa banyak?"
+jumlah = gets.chomp.to_i
+p "pesanan kamu adalah #{menu}, totalnya #{jumlah * 100} dolar"
+
+# upcasess
+
+# downcases
+
+# swapcase
+
+# reverse
+
+# strip 
+
+# split
