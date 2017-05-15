bdc507107820b17b1e0dfcab7fa7b1ab8d42f9aa
Andrian <flyingstarlai@gmail.com>
2017-05-15 20:38:53 +0800

    boolean n number

---
+++ boolean.rb
@@ -0,0 +1,14 @@
+# BOOLEAN: TRUE / FALSE
+
+# < > == != <= >=
+# p 12 < 2
+# p 12 == 12
+# p 10 > 2
+# p 12 != 3
+# p 12 <= 12
+
+
+# odd? even?
+a = 10
+p a.odd?
+p 14.even?
---
+++ number.rb
@@ -0,0 +1,34 @@
+# PEMDA
+# p 2 + 2
+# p 3 - 2
+# p 4 * 4
+# p 4 / 2
+# p 3 ** 2
+# p 3 ** 3
+# p  (2 + 2) * 2
+
+# modulus %
+# p 10 % 4
+
+
+# to_i to_f
+# a = "2.33"
+# b = 3
+# p a.to_i + b
+# p a.to_f + b
+
+# next
+# a = 10
+# p a.next
+
+# floor ceil round abs
+# a = 12.44343
+# p a.floor
+# p a.ceil
+# p a.round(3)
+
+# += -= *= /=
+a = 3
+b = a + 4
+a += 4
+p a
