0f3c6a02649a8934660db8e437a3165c58d6c6a0
Andrian <flyingstarlai@gmail.com>
2017-05-22 19:49:42 +0800

    conditonal elsif

--- conditional.rb
+++ conditional.rb
@@ -1,6 +1,21 @@
+
+angka = 2
+if angka < 10
+    puts "Angka kecil!"
+elsif angka < 50
+    puts "Angka Sedang"
+else
+    puts "Angka Besar"
+end
+
+
+
+# another example conditionals
+
 if 5 < 6
     puts "benar"
 end
+
 word = "martabak"
 if word.length == 8
     puts "word punya 8 huruf"
