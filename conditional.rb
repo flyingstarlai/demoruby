4d540011037694faa2d43b23e887abc3566996fa
Andrian <flyingstarlai@gmail.com>
2017-05-22 20:09:51 +0800

    boolean logic

--- conditional.rb
+++ conditional.rb
@@ -1,28 +1,67 @@
 
-angka = 2
-if angka < 10
-    puts "Angka kecil!"
-elsif angka < 50
-    puts "Angka Sedang"
-else
-    puts "Angka Besar"
-end
+# angka = 2
+# if angka < 10
+#     puts "Angka kecil!"
+# elsif angka < 50
+#     puts "Angka Sedang"
+# else
+#     puts "Angka Besar"
+# end
 
 
 
 # another example conditionals
 
-if 5 < 6
-    puts "benar"
-end
+# if 5 < 6
+#     puts "benar"
+# end
 
-word = "martabak"
-if word.length == 8
-    puts "word punya 8 huruf"
-end
-if word.include?("mar")
-    puts "word mengandung \"mar\""
-end
-if 5.odd?
-    puts "bilangan ganjil"
-end
+# word = "martabak"
+# if word.length == 8
+#     puts "word punya 8 huruf"
+# end
+# if word.include?("mar")
+#     puts "word mengandung \"mar\""
+# end
+# if 5.odd?
+#     puts "bilangan ganjil"
+# end
+
+# def ganjil_apa_genap(angka)
+#     if angka.odd?
+#         puts "Bilangan ganjil"
+#     elsif angka.even?
+#         puts "Bilangan genap"
+#     end
+# end
+
+# ganjil_apa_genap(7)
+# ganjil_apa_genap(6)
+# ganjil_apa_genap(6787)
+
+angka = 10
+buah = "jeruk"
+enak = true
+
+# AND -> &&
+
+# if angka == 11 && buah == "jeruk"
+#     puts "benar"
+# else
+#     puts "salah"
+# end
+
+# OR -> ||
+# if angka == 11 || buah == "jeruk"
+#     puts "benar"
+# else
+#     puts "salah"
+# end
+
+# NOT -> !
+puts !enak
+
+
+
+
+
